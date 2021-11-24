ee79972ecaff656507e33a5e19566757b8f207cc
VolodymyrMishchenko <mischenkoevt@gmail.com>
2021-11-24 20:09:08 +0200

    створив файл 5.ру

---
+++ dz_3/5.py
@@ -0,0 +1,13 @@
+x = int(input("x = "))
+y = int(input("y = "))
+if x > y:
+    z = x - y
+    print("х бiльше нiж у на", z)
+elif x < y:
+    z = y - x
+    print("y бiльше нiж x на", z)
+elif x == y:
+    z = x = y
+    print("х дорiвнює", z)
+
+  
