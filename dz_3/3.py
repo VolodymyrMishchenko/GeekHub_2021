d7790870d20312954755e2e512b2884c60667584
VolodymyrMishchenko <mischenkoevt@gmail.com>
2021-11-25 14:20:35 +0200

    виправив

--- dz_3/3.py
+++ dz_3/3.py
@@ -1,4 +1,16 @@
-# Написати скрипт, який видалить пусті елементи із списка. Список можна "захардкодити".
-user_list = [(), (), ('',), ('a', 'b'), {}, ('a', 'b', 'c'), ('d'), '', []]
-my_list = [x for x in user_list if x]
-print(my_list)
+# Написати функцiю season, яка приймає один аргумент — номер 
+# мiсяця (вiд 1 до 12), яка буде повертати пору року, якiй цей мiсяць 
+# належить (зима, весна, лiто або осінь)
+
+def season(month):
+    if month in [12, 1, 2]:
+        return 'Зима'
+    elif month in [3, 4, 5]:
+        return 'Весна'
+    elif month in [6, 7, 8]:
+        return 'Літо'
+    elif month in [9, 10, 11]:
+        return 'Осінь'
+    return None
+result = season(int(input('Введіь місяць: ')))
+print(result)
