d51bbfcbe52d9089c6e8449d2dd9facf4bf76588
VolodymyrMishchenko <mischenkoevt@gmail.com>
2021-11-25 15:20:43 +0200

    виправив

--- dz_3/2.py
+++ dz_3/2.py
@@ -4,5 +4,11 @@
 from_year = int(input("Введите год от: "))
 to_year = int(input("Введите год до: "))
 for year in range(from_year, to_year + 1):
-    if year % 4 == 0:
-        print(year)
+    if year % 400 == 0:
+        print("%d високосный" %year)
+    elif year % 100 == 0:
+        print("%d не високосный" %year)
+    elif year % 4 == 0:
+        print("%d високосный" %year)
+    else:
+        print("%d не високосный" %year)
