7c85ee191659e1e187735fedd2ecedb6a123e2af
VolodymyrMishchenko <mischenkoevt@gmail.com>
2021-11-26 15:31:23 +0200

    виправив

--- dz_3/4.py
+++ dz_3/4.py
@@ -4,17 +4,22 @@
 # повернутий ними результат та також повертає результат. Таким чином 
 # ми будемо викликати 1 функцiю, а вона в своєму тiлi ще 3.
 
-def printinfo(user_name):
+def func_1():
     user_name = input("Ваше ім'я?:")
     return user_name
-result = printinfo('user_name')
-def printinfo(user_age):
+
+def func_2():
     user_age = input("Ваш вік?:")
     return user_age
-result = printinfo('user_age')
-def printinfo(user_city):
+
+def func_3():
     user_city = input("Ваше місто?:")
     return user_city
-result = printinfo('user_city')
 
-print(result)
+def func_4():
+    my_name = func_1()
+    my_age = func_2()
+    my_cyti = func_3()
+    print(f'My name is {my_name}. I am {my_age}. I live in {my_cyti}')
+    
+func_4()
