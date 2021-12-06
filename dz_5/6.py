aef232bfd467a2c13c294b5b4e10f412e9abc0c6
Volodymyr <mischenkoevt@gmail.com>
2021-12-06 16:21:35 +0200

    виправив

--- dz_5/6.py
+++ dz_5/6.py
@@ -6,16 +6,30 @@
 # https://docs.python.org/3/library/stdtypes.html#range
 
 def custom_range(start, stop=0, step=1):
-    if stop == 0:
-        stop = start
-        start = 0
-    while True:
-        yield start
-        start = start + step
-        if start > stop or start == stop:
-            break
-        
+    if step > 0:
+        if start and not stop:
+            stop = start
+            start = 0
+        while start < stop:
+            yield start
+            start += step
+    else:
+        if start > stop:
+            while start > stop:
+                yield start
+                start += step
+
+
+     
+     
+print(list(custom_range(20)))
+print(list(custom_range(0, 20)))
 print(list(custom_range(0, 20, 2)))
+print(list(custom_range(20, 0, -2)))
+print(list(custom_range(20, 0)))
+print(list(custom_range(-20)))
+print(list(custom_range(0, 20, -2)))
+
 print(list(range(0, 20, 2)))
 
 for i in custom_range(0, 20, 2):
